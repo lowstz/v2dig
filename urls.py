949f4e82b6bb03db16bc4637666f1ea2d5df761f
Lowstz Chen <lowstz@gmail.com>
2012-12-03 10:56:35 +0800

    路由更新

--- urls.py
+++ urls.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 
-from handlers import handler
+from handlers import account
+from handlers import topic
+from handlers import node
+from handlers import member
 
 __all__ = ['handlers', 'ui_modules']
 
 handlers=[]
-handlers.extend(handler.handlers)
+handlers.extend(account.handlers)
+handlers.extend(topic.handlers)
+handlers.extend(node.handlers)
+handlers.extend(member.handlers)
+
 ui_modules={}
+ui_modules.update(**topic.ui_modules)
+ui_modules.update(**member.ui_modules)
